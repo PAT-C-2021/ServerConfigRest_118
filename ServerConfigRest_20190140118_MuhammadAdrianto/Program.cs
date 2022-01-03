a57625cb90bf6b450af2d193aa4e7b58424f5598
Muhammad Adrianto <isfan.diar123@gmail.com>
2022-01-03 22:34:58 +0700

    Add project files.

---
+++ ServerConfigRest_20190140118_MuhammadAdrianto/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_20190140118_Muhammad_Adrianto_Fakhar_Isfandiyar;
+
+namespace ServerConfigRest_20190140118_MuhammadAdrianto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server is Ready..");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
